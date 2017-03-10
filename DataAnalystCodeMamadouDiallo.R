3ba38f4a2a3b23e80836a533dbe852df2b4a48e9
Mamadou Diallo <mams.diallo@gmail.com>
2017-03-10 23:47:19 +0100

    modify percentage: fixed

--- DataAnalystCodeMamadouDiallo.R
+++ DataAnalystCodeMamadouDiallo.R
@@ -1,5 +1,6 @@
 #libraries
 library("dplyr")
+library("ggplot2")
 
 # import data
 data <- read.csv("data-15aa4675580.csv")
@@ -10,11 +11,15 @@ df <- tbl_df(data)
 # Analyze data
 summary(df)
 str(df)
+ggplot(df,aes(x = Colour,y = Vals))+geom_boxplot()
 
 # convert factors as characters
 df %>% mutate_if(is.factor, as.character) -> df
 # to lower case 
 df <- mutate(df,Colour = tolower(Colour))
+
+#Analysis
+ggplot(df,aes(x = Colour,y = Vals))+geom_boxplot()
 
 # set pass to true when it belongs to [13,14]
 df %>% mutate(pass = (Vals<=14)&(Vals>=13)) -> df
@@ -30,6 +35,11 @@ df %>% arrange(Colour,pass) -> df
 df %>%
   mutate(Colour=replace(Colour, Colour=="purpal", "purple")) -> df
 
+#Analysis
+ggplot(df,aes(x = Colour,y = Vals))+geom_boxplot()
+# count Colours
+table(df$Colour)
+
 # capitalize first letter of colour names
 df %>%
   mutate(Colour=replace(Colour, Colour=="purple", "Purple")) %>%
@@ -37,8 +47,12 @@ df %>%
   mutate(Colour=replace(Colour, Colour=="blue", "Blue")) %>%
   mutate(Colour=replace(Colour, Colour=="green", "Green")) -> df
 
+#Analysis
+ggplot(df,aes(x = Colour,y = Vals))+geom_boxplot()
+
 # Count nb part per Colours
 table(df$Colour)
+
 
 # store nb of rows in Variable
 N = nrow(df)
@@ -54,9 +68,16 @@ df_result <- df_nb_part
 by_colour_pass <- group_by(df,Colour,pass)
 
 # compute percent pass (when pass is TRUE)
-by_colour_pass %>% filter(pass==TRUE) %>% summarise(Percent_Pass = 100*n()/N) %>% select(Colour,Percent_Pass)-> df_percent_pass
+df %>% group_by(Colour,pass) %>% 
+  summarise(Number_of_Parts = n()) %>% 
+  mutate(Percent_Pass = 100*Number_of_Parts/sum(Number_of_Parts)) %>% 
+  filter(pass == TRUE) %>% 
+  select(Colour, Percent_Pass) -> df_percent_pass
+#by_colour_pass %>% filter(pass==TRUE) %>% summarise(Percent_Pass = 100*n()/N) %>% select(Colour,Percent_Pass)-> df_percent_pass
+
 # store results
 df_result$Percent_Pass = df_percent_pass$Percent_Pass
+
 
 # compute mean length and variance of length
 by_colour %>% summarise(Mean_length = mean(Vals),Variance_of_length = var(Vals)) -> df_mean_var
@@ -65,8 +86,5 @@ by_colour %>% summarise(Mean_length = mean(Vals),Variance_of_length = var(Vals))
 df_result$Mean_length = df_mean_var$Mean_length
 df_result$Variance_of_length = df_mean_var$Variance_of_length
 
-#change variable names
-
 # save in csv file
 write.csv(df_result, file = "result.csv")
-
